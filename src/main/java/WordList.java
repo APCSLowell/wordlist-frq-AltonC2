3531551e783a403a5fc78648ceaabd34faaf65e3
AltonC2 <143027301+AltonC2@users.noreply.github.com>
2024-04-03 12:04:20 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,22 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    for( int i = 0; i < myList.size();i++){
+      if( myList.get(i).length==len)
+        count++;
+      else
+        count=count;
+    }
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
+    for ( int i = myList.size()-1;i>=0;i--){
+      if ( myList.get(i).length==len)
+        myList.remove(i);
+    else
+        myList = myList;
+    }
   }
 }
