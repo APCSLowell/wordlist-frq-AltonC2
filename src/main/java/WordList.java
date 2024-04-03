8b5e5e5a5f3fc0f65b74c480ab937b48a24a80d6
AltonC2 <143027301+AltonC2@users.noreply.github.com>
2024-04-03 12:13:10 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -22,7 +22,7 @@ public class WordList
 
   public void removeWordsOfLength(int len)
   {
-    for ( int i = myList.size()-1;i>=0;i--){
+    for ( int i = myList.size();i>=0;i--){
       if ( myList.get(i).length()==len)
         myList.remove(i);
     else
